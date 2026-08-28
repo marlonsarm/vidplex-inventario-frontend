1c7bb3bb083dff348b29ade23dec696b5ea7d296
Marlon <marlonsarm10@gmail.com>
2026-08-28 08:27:11 -0500

    Agregar Dockerfile, nginx y build web para Railway (sin apk)

---
+++ build/jni/intermediates/cxx/RelWithDebInfo/501u6e1t/logs/x86_64/build_command_jni.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Marlon\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\Marlon\\AppData\\Local\\Pub\\Cache\\hosted\\pub.dev\\jni-1.0.3\\android\\.cxx\\RelWithDebInfo\\501u6e1t\\x86_64" ^
+  jni
