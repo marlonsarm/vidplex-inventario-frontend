3749e4b347f23e6e2c8dbd1fb200bb007ffb3393
Marlon <marlonsarm10@gmail.com>
2026-08-28 11:44:21 -0500

    Recompilar build web con autocomplete de productos

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1706324521" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "821360013" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
