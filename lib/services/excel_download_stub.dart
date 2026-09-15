0d0c42faed0e9553f4525d91bdeac90bea449a16
Marlon <marlonsarm10@gmail.com>
2026-09-15 07:59:49 -0500

    v1.0.10: corrige descarga de Excel en Windows con file_picker

--- lib/services/excel_download_stub.dart
+++ lib/services/excel_download_stub.dart
@@ -1,5 +1,13 @@
 import 'dart:typed_data';
+import 'package:file_picker/file_picker.dart';
 
 Future<void> descargarExcelWeb(Uint8List bytes, String nombreArchivo) async {
-  // No aplica en Android/Windows, solo se usa en la version web.
+  await FilePicker.saveFile(
+    fileName: nombreArchivo,
+    bytes: bytes,
+    mimeType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    dialogTitle: 'Guardar Excel',
+    type: FileType.custom,
+    allowedExtensions: ['xlsx'],
+  );
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker_darwin
 import file_selector_macos
 import mobile_scanner
 import package_info_plus
@@ -12,6 +13,7 @@ import shared_preferences_foundation
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
